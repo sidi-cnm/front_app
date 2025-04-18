bc4c5f05f466d8102c58be35f7e93a26e6a791c9
sidi-cnm <sdeiebeidy@gmail.com>
2025-04-18 00:36:09 +0000

    feat:structure

---
+++ src/types/patient.ts
@@ -0,0 +1,10 @@
+// src/types/patient.ts
+export interface Patient {
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    enrollNumber: string;
+    lastVisit: string;
+    avatar?: string;
+  }
